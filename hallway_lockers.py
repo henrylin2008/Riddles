6d579b5260eb1e0829c11953aeeb9f472d4cfe32
Henry Lin <henry.lin@appdirect.com>
2018-12-21 21:28:07 -0800

    Upload everything

---
+++ hallway_lockers.py
@@ -0,0 +1,19 @@
+Jugs of Water - SOLUTION
+Problem Statement
+You have a five gallons jug and a three gallons jug, and an unlimited supply of water (but no measuring cups) How would you come up with exactly four gallons of water?
+
+Solution
+This problem has a cameo in the movie Die Hard 3. The solution is below:
+
+In [3]:
+from IPython.display import Image
+Image(url='http://mindyourdecisions.com/blog/wp-content/uploads/2013/02/water-jug-riddle-1.png')
+Out[3]:
+
+Hopefully your interviews are not as stressful as this:
+
+In [5]:
+from IPython.display import YouTubeVideo
+YouTubeVideo('BVtQNK_ZUJg')
+Out[5]:
+Yippee Ki Yay
